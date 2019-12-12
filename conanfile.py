ea2e908bc9bfae34525a23f0197981bea1f3bf5a
Rohan Sahay <rohansahay@elear.solutions>
2019-12-12 14:19:31 +0530

    add make distclean check

--- conanfile.py
+++ conanfile.py
@@ -471,6 +471,8 @@ class OpenSSLConan(ConanFile):
                 self._run_make(targets=["install_sw"], parallel=False)
 
     def build(self):
+        if os.path.isfile("../libcrypto.a"):
+            self.run("cd .. && make distclean")
         with tools.vcvars(self.settings) if self.settings.compiler == "Visual Studio" else tools.no_op():
             env_vars = {"PERL": self._perl}
             if self.settings.compiler == "apple-clang":
